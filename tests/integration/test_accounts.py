f4d8feacc88acf3e5612e5cf3a3964c6a3df52f8
Saurabh Mishra <saurab.mish@gmail.com>
2020-12-24 18:43:36 -0800

    client.get, client.post, and client.patch complete; coverage 97%

--- tests/integration/test_accounts.py
+++ tests/integration/test_accounts.py
@@ -60,14 +60,67 @@ def test_get_user_status(client):
     assert response.status_code == 201
 
 
-def test_update_user_location(client):
-    """TODO."""
-    pass
+def test_no_user_message(client):
+    response = client.get('/api/v1/user/get/Saurabh')
+    data = json.loads(response.get_data(as_text=True))
+    assert 'User not found' in data['message']
+
+
+def test_no_user_status(client):
+    response = client.get('/api/v1/user/get/Saurabh')
+    data = json.loads(response.get_data(as_text=True))
+    assert data['status'] == 406
 
 
 def test_update_user_location_status(client):
-    """TODO."""
-    pass
+    url = '/api/v1/user/update-location/Hannah'
+    user_data = {
+        "name": "Hannah",
+        "location": "Santa Ana",
+        "gender": "F"
+    }
+    response = client.patch(url, data=json.dumps(user_data),
+                            content_type='application/json')
+    assert response.status_code == 205
+
+
+def test_update_user_location_name(client):
+    url = '/api/v1/user/update-location/Hannah'
+    user_data = {
+        "name": "Hannah",
+        "location": "Santa Ana",
+        "gender": "F"
+    }
+    response = client.patch(url, data=json.dumps(user_data),
+                            content_type='application/json')
+    data = json.loads(response.get_data(as_text=True))
+    assert "Hannah" in data["name"]
+
+
+def test_update_user_location(client):
+    url = '/api/v1/user/update-location/Hannah'
+    user_data = {
+        "name": "Hannah",
+        "location": "Santa Ana",
+        "gender": "F"
+    }
+    response = client.patch(url, data=json.dumps(user_data),
+                            content_type='application/json')
+    data = json.loads(response.get_data(as_text=True))
+    assert "Santa Ana" in data["location"]
+
+
+def test_update_user_location_gender(client):
+    url = '/api/v1/user/update-location/Hannah'
+    user_data = {
+        "name": "Hannah",
+        "location": "Santa Ana",
+        "gender": "F"
+    }
+    response = client.patch(url, data=json.dumps(user_data),
+                            content_type='application/json')
+    data = json.loads(response.get_data(as_text=True))
+    assert "F" in data["gender"]
 
 
 def test_delete_user_status(client):
